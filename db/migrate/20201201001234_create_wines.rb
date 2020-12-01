b09907779aad4282c820bcf3feeda39d8f38814d
kennja05 <jacobkenny05@gmail.com>
2020-12-01 15:37:21 -0500

    create database schema

--- db/migrate/20201201001234_create_wines.rb
+++ db/migrate/20201201001234_create_wines.rb
@@ -2,8 +2,10 @@ class CreateWines < ActiveRecord::Migration[6.0]
   def change
     create_table :wines do |t|
       t.string :name
-      t.rating :integer
+      t.integer :rating
+      t.float :price
       t.text :review
+      t.datetime :purchase_date
       t.timestamps
     end
   end
