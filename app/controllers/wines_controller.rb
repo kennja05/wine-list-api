799474d9507afca67893d2271a43bdd50495aac9
kennja05 <jacobkenny05@gmail.com>
2020-12-01 17:49:47 -0500

    update seedfile. add cors gem. add wine controller methods

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -1,14 +1,13 @@
 class WinesController < ApplicationController
 
-
     def index
         wines = Wine.all
+        render json: wines
     end
 
     def show
-
+        wine = Wine.find(params['id'])
+        render json: wine
     end
-
-
 
 end
